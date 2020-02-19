8286549e0e22ac6b78e060ace67eae47ea6fda75
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-02-19 11:43:44 +0100

    Add project files.

---
+++ RPS/ViewModel/Converters/ResultToPicture.cs
@@ -0,0 +1,41 @@
+﻿using RPS.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace RPS.ViewModel.Converters
+{
+    class ResultToPicture: IValueConverter
+    {
+        public BitmapImage Rock { get; set; }
+        public BitmapImage Paper { get; set; }
+        public BitmapImage Scissors { get; set; }
+
+        public BitmapImage Default { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is RPSResult)
+            {
+                switch((int) value)
+                {
+                    case 0: { return Default; }
+                    case 1: { return Rock; }
+                    case 2: { return Paper; }
+                    case 3: { return Scissors; }
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RPS/ViewModel/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using RPS.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private RPSResult _player;
+        private RPSResult _computer;
+
+        private Random _rand;
+
+        public ParametrizedRelayCommand Play { get; private set; }
+
+        public MainViewModel()
+        {
+            Player = RPSResult.None;
+            Computer = RPSResult.None;
+            _rand = new Random();
+            Play = new ParametrizedRelayCommand(
+                (param) =>
+                {
+                    if (param is /*RPSResult*/ string)
+                    {
+                        switch (param)
+                        {
+                            //case RPSResult.None: Player = RPSResult.None; break;
+                            case /*RPSResult.Rock*/ "1": Player = RPSResult.Rock; break;
+                            case /*RPSResult.Scissors*/ "2": Player = RPSResult.Scissors; break;
+                            case /*RPSResult.Paper*/ "3": Player = RPSResult.Paper; break;
+                            default: Player = RPSResult.None; break;
+                        }
+                        Computer = (RPSResult)_rand.Next(3) + 1;
+                    }
+                },
+                () => true
+            );
+        }
+
+        public RPSResult Player
+        {
+            get
+            {
+                return _player;
+            }
+            set
+            {
+                _player = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public RPSResult Computer
+        {
+            get
+            {
+                return _computer;
+            }
+            set
+            {
+                _computer = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
